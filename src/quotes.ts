cedae8310caf30ecb01638c63df4b44bbf1bbc79
Ovie Revaldi <ovierevaldi@gmail.com>
2025-01-20 14:49:53 +0700

    Create Quite Generator Project

---
+++ src/quotes.ts
@@ -0,0 +1,12 @@
+export const quotes = [
+    "The only way to do great work is to love what you do. – Steve Jobs",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. – Winston Churchill",
+    "Life is what happens when you're busy making other plans. – John Lennon",
+    "Do what you can, with what you have, where you are. – Theodore Roosevelt",
+    "You miss 100% of the shots you don’t take. – Wayne Gretzky",
+    "Be the change that you wish to see in the world. – Mahatma Gandhi",
+    "It does not matter how slowly you go as long as you do not stop. – Confucius",
+    "Happiness is not something ready-made. It comes from your own actions. – Dalai Lama",
+    "In the middle of every difficulty lies opportunity. – Albert Einstein",
+    "Your time is limited, so don’t waste it living someone else’s life. – Steve Jobs"
+  ];
